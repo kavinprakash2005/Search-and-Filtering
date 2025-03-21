7a841561848b90ef360504b4ecdfca5c21c59e88
kavinprakash2005 <rajendrankandasamy487@gmail.com>
2025-03-21 07:55:47 +0530

    c reate recommended section

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ function App() {
   return (
     <>
     <Navigation/>
-    <Products/>
+    
     <Recommended/>
+    <Products/>
       
   </>
     
--- src/Recommended/Recommended.jsx
+++ src/Recommended/Recommended.jsx
@@ -1,8 +1,21 @@
 
 import './Recommended.css';
- function Recommended() {
+function Recommended() {
   return (
-    <div>Recommended</div>
-  )
+    <div>
+      <h2 className='recommended-title'>Recommended</h2>
+      <div className="recommended-flex">
+        <button className='btns'>All products</button>
+        <button className='btns'>Realme</button>
+        <button className='btns'>redmi</button>
+        <button className='btns'>iqooo</button>
+        <button className='btns'>Oneplus</button>
+        <button className='btns'>iphone</button>
+        <button className='btns'>Vivo</button>
+        <button className='btns'>Samsung</button>
+        <button className='btns'>infinix</button>
+      </div>
+    </div>
+  ) 
 }
-export default Recommended;
+export default Recommended;  
